15aefeca0f6eeea0098b82ee940150fa5695e439
lapty <charles.qh.nguyen@gmail.com>
2014-08-09 10:22:23 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+  $("#repoButton").click(function(){
+    event.preventDefault();
+    $("#events").css("display", "none");
+    $("#repos").css("display", "block");
+  });
+
+  $("#eventButton").click(function(){
+    event.preventDefault();
+    $("#events").css("display", "inline-block");
+    $("#repos").css("display", "none");
+
+  });
+
+  var homePage = {
+
+      init: function() {
+        homePage.initStyling();
+        homePage.initEvents();
+      },
+      initStyling: function() {
+        homePage.renderSide($(".sideBar"));
+        homePage.renderRepo($(".repoBar"));
+        homePage.renderEvent($(".eventBar"));
+
+
+      },
+      initEvents: function() {
+
+      },
+      render: function(template, data, $target) {
+          var tmpl = _.template(template, data);
+
+          $target.append(tmpl);
+
+      },
+      renderSide: function($target) {
+        homePage.render($("#sideTmpl").html(), masterData, $target);
+
+      },
+      renderRepo: function($target) {
+        homePage.render($("#repoTmpl").html(), masterData, $target);
+
+      },
+
+      renderEvent: function($target) {
+        homePage.render($("#eventTmpl").html(), masterData, $target);
+
+      }
+
+  };
+
+homePage.init();
+
+
+
+
+
+});
